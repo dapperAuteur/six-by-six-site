a0fb7b31821436b54119acd22d8a1bd542291b14
dapperauteur <a@awews.com>
2018-08-17 00:41:22 -0400

    add facebook login package

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import Main from './Main';
 import NavBar from './../components/NavBar/NavBar';
+import Facebook from './../components/Facebook';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
+        
         <h1>SixBySix</h1>
+        <Facebook />
 
         <Main />
       </div>
