50aa5df6a24263782066ae701e91743362a8eaea
dapperauteur <a@awews.com>
2018-08-17 00:58:21 -0400

    remove fb docs button, add picture to page

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -25,6 +25,7 @@ export default class Facebook extends Component {
     };
   render() {
       let fbContent;
+      let picture = this.state.picture;
 
       if (this.state.isLoggedIn) {
           console.log(this.state); 
@@ -36,28 +37,13 @@ export default class Facebook extends Component {
                 autoLoad={ true }
                 fields="name,email,picture"
                 onClick={ this.componentClicked }
-                callback={ this.responseFacebook }
-                className="fb-login-button"
-                data-max-rows="1"
-                data-size="large"
-                data-button-type="continue_with"
-                data-show-faces="true"
-                data-auto-logout-link="false"
-                data-use-continue-as="true" />
+                callback={ this.responseFacebook } />
             )
       }
     return (
       <div>
         { fbContent }
-        <div
-            className="fb-login-button"
-            data-max-rows="1"
-            data-size="large"
-            data-button-type="continue_with"
-            data-show-faces="true"
-            data-auto-logout-link="false"
-            data-use-continue-as="true">
-        </div>
+        { picture }
       </div>
     )
   }
