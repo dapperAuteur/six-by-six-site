5dd7b78fe3a00161ad59ab44e0059e6efe5beb78
dapperauteur <a@awews.com>
2018-08-17 00:53:16 -0400

    add facebook button from docs

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -13,25 +13,51 @@ export default class Facebook extends Component {
 
     responseFacebook = response => {
         console.log(response);
+        this.setState({
+            isLoggedIn: true,
+            id: response.id,
+            userID: response.userId,
+            name: response.name,
+            email: response.email,
+            picture: response.picture.data.url,
+            accessToken: response.accessToken
+        })
     };
   render() {
       let fbContent;
 
       if (this.state.isLoggedIn) {
+          console.log(this.state); 
         fbContent = null;
       } else {
           fbContent = (
             <FacebookLogin
                 appId="1645178768938930"
-                autoLoad={true}
+                autoLoad={ true }
                 fields="name,email,picture"
-                onClick={this.componentClicked}
-                callback={this.responseFacebook} />
+                onClick={ this.componentClicked }
+                callback={ this.responseFacebook }
+                className="fb-login-button"
+                data-max-rows="1"
+                data-size="large"
+                data-button-type="continue_with"
+                data-show-faces="true"
+                data-auto-logout-link="false"
+                data-use-continue-as="true" />
             )
       }
     return (
       <div>
         { fbContent }
+        <div
+            className="fb-login-button"
+            data-max-rows="1"
+            data-size="large"
+            data-button-type="continue_with"
+            data-show-faces="true"
+            data-auto-logout-link="false"
+            data-use-continue-as="true">
+        </div>
       </div>
     )
   }
